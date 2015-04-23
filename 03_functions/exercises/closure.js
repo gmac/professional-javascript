b7a57a57f82f91c69b501025e2bb62bbc3bfaa60
Greg MacWilliam <greg.macwilliam@voxmedia.com>
2015-04-23 15:57:49 -0400

    Adding closure demo.

---
+++ 03_functions/exercises/closure.js
@@ -0,0 +1,36 @@
+// Kat's App
+(function() {
+  // Kat's Code...
+  var cookieOven = 'true';
+  var bakingSheet = true;
+  var chocolateChips = true;  
+})();
+
+
+// Walter's App
+(function() {
+  // Walter White's Code
+  var cookieOven = true;
+  var chemicals = true;
+  var bakingSheets = true;
+})();
+
+
+// Kat :-)
+var katPhotos = true;
+
+function washington() {
+  var monuments = true;
+
+  function brookland() {
+    var catholicU = true;
+    // Pam :-)
+  }
+
+  function shaw() {
+    var bigBear = true;
+    // Steve :-)
+  }
+
+  // Ngan :-)
+}
